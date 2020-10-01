56b5c2f417805e7b257dfef82b9901f381bedd84
Aman Gupta <aman.gupta2024@gmail.com>
2020-10-01 13:40:43 +0530

    web to worker

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ from os import environ
 # app = Flask(__name__)
 # app.run(environ.get('PORT'))
 
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello from Python!"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+# app = Flask(__name__)
+#
+# @app.route("/")
+# def hello():
+#     return "Hello from Python!"
+#
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host='0.0.0.0', port=port)
 
 load_dotenv()
 TOKEN = environ['DISCORD_TOKEN']
